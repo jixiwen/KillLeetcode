b911d9c6b0de33aba6fc904d03e9b53e90e10485
jixiwen <1270743844@qq.com>
2024-09-07 21:57:46 +0800

    .

---
+++ src/main/java/org/example/lc_1030.java
@@ -0,0 +1,21 @@
+package org.example;
+
+/**
+ * @see <a href="https://leetcode.cn/problems/matrix-cells-in-distance-order/">1030. 距离顺序排列矩阵单元格</a>
+ */
+public class lc_1030 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        int[][] res = new int[rows * cols][2];
+        for (int i = 0; i < rows; i++) {
+
+        }
+        return res;
+
+    }
+}
